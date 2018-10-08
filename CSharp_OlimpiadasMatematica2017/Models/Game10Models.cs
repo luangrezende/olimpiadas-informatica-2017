dbbd5104ffadf46503385cdb0e0f4f7bb30dbe7b
Gomes <Lluangrezende@gmail.com>
2018-10-08 12:44:42 -0300

    update metodos

---
+++ CSharp_OlimpiadasMatematica2017/Models/Game10Models.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OlimpiadasMatematica2017.Models
+{
+    class Game10Models
+    {
+        //POSICOES
+        public int disco { get; set; }
+        public int aviao { get; set; }
+        public int qtdPosicoes { get; set; }
+    }
+}
